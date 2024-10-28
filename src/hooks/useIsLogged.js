66924710b5bac87d24f5ae1a48586691471d0c3c
jimmycoco <jimmy19990211@gmail.com>
2024-10-28 23:47:30 +0800

    新增檢測登入狀態的函式，並用首頁測試

---
+++ src/hooks/useIsLogged.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+function useIsLogged() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        // 從 localStorage 取得 user 資料
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            try {
+                const parsedUser = JSON.parse(storedUser);
+                const token = parsedUser && parsedUser.data && parsedUser.data.token
+                console.log(parsedUser.data)
+                if (token) {
+                    setIsLoggedIn(true);
+                    setUser(parsedUser.data);
+                }
+            } catch (error) {
+                console.error("Failed to parse user data from localStorage:", error);
+            }
+        }
+    }, []);  // 空依賴陣列確保只在初次渲染時執行
+
+    return { isLoggedIn, user };
+}
+
+export default useIsLogged;
