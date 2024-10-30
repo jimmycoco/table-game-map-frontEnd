0190f8d764a0e2756c997f95ecd15e25e3b305ee
jimmycoco <jimmy19990211@gmail.com>
2024-10-30 16:37:33 +0800

    新增isLogged並用首頁測試

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import "./Home.css"
+import useIsLogged from "../hooks/useIsLogged";
+
+
 
 
 const HomePage = props => {
-    return <div className="homepage"></div>;
+    const { isLoggedIn, user } = useIsLogged();
+
+    return <div className="homepage">
+        {isLoggedIn
+            ? (<div>歡迎回來, {user.name}!</div>)
+            : (<div>請登入以繼續。</div>)
+        }
+    </div>;
 
 }
 export default HomePage;
